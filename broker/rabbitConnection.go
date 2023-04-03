ad03f22baa90ac83832674eef5b7cb347c21c053
eminoz <eminoz@yahoo.com>
2023-04-04 01:44:42 +0300

    v1

--- broker/rabbitConnection.go
+++ broker/rabbitConnection.go
@@ -1,6 +1,7 @@
 package broker
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/streadway/amqp"
@@ -10,17 +11,17 @@ var Channel *amqp.Channel
 var Conn *amqp.Connection
 
 func RabbitConnection() {
-	conn, err := amqp.Dial("amqp://" + "myuser" + ":" + "mypassword" + "@" + "10.105.66.16" + ":" + "5672" + "/")
+	conn, err := amqp.Dial("amqp://" + "myuser" + ":" + "mypassword" + "@" + "172.17.0.7" + ":" + "5672" + "/")
 	// conn, err := amqp.Dial("amqp://" + "eminoz" + ":" + "eminoz" + "@" + "localhost" + ":" + "5672" + "/")
 
 	if err != nil {
 		log.Fatalf("%s: %s", "Failed to connect to RabbitMQ", err)
 	}
+	fmt.Print("connected")
 	ch, err := conn.Channel()
 	if err != nil {
 		log.Fatal("Failed to open a channel:", err)
 	}
-
 	Channel = ch
 	Conn = conn
 }
